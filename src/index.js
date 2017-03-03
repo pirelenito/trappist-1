cfea688d256254788a17fa2f87b893f7a33952a3
Paulo Ragonha <paulo@ragonha.me>
2017-03-03 12:28:10 +0100

    Render TRAPPIST-1 to scale 🌎☀️
    source: https://en.wikipedia.org/wiki/TRAPPIST-1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
+import PlanetarySystem from './planetary-system'
 
-render(<h1>Hello</h1>, document.getElementById('root'))
+const style = {
+  background: '#10052b',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  '-webkit-tap-highlight-color': 'rgba(0,0,0,0)'
+}
+
+render(<svg style={style} width='100%' height='100%'>
+  <g transform={'translate(40, 40)'}>
+    <PlanetarySystem />
+  </g>
+</svg>, document.getElementById('root'))
---
+++ src/planetary-system.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const EARTH_RADIUS = 0.006371
+const SUN_RADIUS = 109 * EARTH_RADIUS
+
+const SCALE = 500
+
+const star = {
+  radius: SUN_RADIUS * 0.11
+}
+
+const planets = [
+  { name: 'b', radius: 1.086 * EARTH_RADIUS, distance: 1.66 },
+  { name: 'c', radius: 1.056 * EARTH_RADIUS, distance: 2.28 },
+  { name: 'd', radius: 0.772 * EARTH_RADIUS, distance: 3.14 },
+  { name: 'e', radius: 0.918 * EARTH_RADIUS, distance: 4.19 },
+  { name: 'f', radius: 1.045 * EARTH_RADIUS, distance: 5.54 },
+  { name: 'g', radius: 1.127 * EARTH_RADIUS, distance: 6.73 }
+]
+
+export default () => {
+  return <g>
+    <Star star={star} />
+    {
+      planets.map((planet) => <Orbit planet={planet} />)
+    }
+  </g>
+}
+
+function Star ({ star }) {
+  return <circle
+    r={star.radius * SCALE}
+    fill='#062a67'
+    strokeWidth='0'
+  />
+}
+
+function Orbit ({ planet }) {
+  const distance = planet.distance * SCALE
+  const radius = planet.radius * SCALE
+
+  return <g
+    style={{transform: `rotate(${0}rad)`}}>
+    <circle
+      r={distance}
+      fill='none'
+      opacity={0.3}
+      stroke='#97005d'
+      strokeWidth='1'
+    />
+
+    <g transform={`translate(${distance}, 0)`}>
+      <circle r={radius} fill='#3fb9dd' />
+    </g>
+  </g>
+}
