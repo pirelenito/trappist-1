60eb6dd3a8a1a9fe5e4963c7f8810b88c7b33df9
Paulo Ragonha <paulo@ragonha.me>
2017-03-03 14:25:03 +0100

    Fix scale of everything (hopefully) 🤷‍♂️

--- src/planetary-system.js
+++ src/planetary-system.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-const EARTH_RADIUS = 0.006371
-const SUN_RADIUS = 109 * EARTH_RADIUS
+const EARTH_RADIUS = 6371 // km
+const SUN_RADIUS = (109 / 2) * EARTH_RADIUS
 
-const SCALE = 2000
+const SCALE = 0.002
 
 const star = {
   radius: SUN_RADIUS * 0.11
 }
 
 const planets = [
-  { name: 'b', radius: 1.086 * EARTH_RADIUS, distance: 1.66 },
-  { name: 'c', radius: 1.056 * EARTH_RADIUS, distance: 2.28 },
-  { name: 'd', radius: 0.772 * EARTH_RADIUS, distance: 3.14 },
-  { name: 'e', radius: 0.918 * EARTH_RADIUS, distance: 4.19 },
-  { name: 'f', radius: 1.045 * EARTH_RADIUS, distance: 5.54 },
-  { name: 'g', radius: 1.127 * EARTH_RADIUS, distance: 6.73 }
+  { name: 'b', radius: 1.086 * EARTH_RADIUS, distance: 1.66 * 1000000 },
+  { name: 'c', radius: 1.056 * EARTH_RADIUS, distance: 2.28 * 1000000 },
+  { name: 'd', radius: 0.772 * EARTH_RADIUS, distance: 3.14 * 1000000 },
+  { name: 'e', radius: 0.918 * EARTH_RADIUS, distance: 4.19 * 1000000 },
+  { name: 'f', radius: 1.045 * EARTH_RADIUS, distance: 5.54 * 1000000 },
+  { name: 'g', radius: 1.127 * EARTH_RADIUS, distance: 6.73 * 1000000 }
 ]
 
 export default () => {
